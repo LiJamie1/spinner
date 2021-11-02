3b74b7de444daed31905361e9dbf0d09ad93fb7f
Jamie Li <lijamie19828@gmail.com>
2021-11-02 23:26:04 +0000

    refactored spinner1

---
+++ spinner2.js
@@ -0,0 +1,47 @@
+const arrPositions = ['|', '/', '-', '\\', '|', '/', '-', '\\'];
+let time = 100
+
+for (const position of arrPositions) {
+  setTimeout(() => {
+    process.stdout.write(`\r ${position}    `);
+  }, time)
+  time += 200
+};
+
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+
+// setTimeout(() => {
+//   for (let i = 0; i < 4; i++) {
+//     if (i === 0) {
+//       process.stdout.write('\r|   ')
+//     }
+//     else if (i === 1) {
+//       process.stdout.write('\r/   ')
+//     }
+//     else if (i === 2) {
+//       process.stdout.write('\r-   ')
+//     }
+//     else if (i === 3) {
+//       process.stdout.write('\r\\   ')
+//       i = 0
+//     }
+//   }
+// });
+
